c1082b29a3c66b83420c1af400af3031218fb4a2
Chandler Tu <chandlertu@chandlertu.com>
2018-10-23 16:50:04 +0800

    Add Dietary api

---
+++ src/main/java/cn/tuchangle/web/DatabaseLoader.java
@@ -0,0 +1,36 @@
+package cn.tuchangle.web;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+  @Autowired
+  private DietaryRepository repository;
+
+  @Override
+  public void run(String... strings) throws Exception {
+    Dietary dietary = new Dietary();
+    dietary.setFood("苹果");
+    dietary.setGram(100);
+    dietary.setInsertTime(new Date());
+    this.repository.save(dietary);
+
+    Dietary dietary2 = new Dietary();
+    dietary2.setFood("香蕉");
+    dietary2.setGram(200);
+    dietary2.setInsertTime(new Date());
+    this.repository.save(dietary2);
+
+    Dietary dietary3 = new Dietary();
+    dietary3.setFood("葡萄");
+    dietary3.setGram(300);
+    dietary3.setInsertTime(new Date());
+    this.repository.save(dietary3);
+  }
+
+}
