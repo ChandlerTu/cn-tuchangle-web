f7266b704a41b22b171d65d71d9efdb6c345d850
Chandler Tu <chandlertu@chandlertu.com>
2018-10-23 16:46:55 +0800

    Format

--- src/main/java/cn/tuchangle/web/CnTuchangleWebApplication.java
+++ src/main/java/cn/tuchangle/web/CnTuchangleWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CnTuchangleWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CnTuchangleWebApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(CnTuchangleWebApplication.class, args);
+  }
 }
