847703c240a2a04005b02214ae4bf339a8b5c721
Chandler Tu <chandlertu@chandlertu.com>
2018-11-14 15:04:04 +0800

    增加根据日期查询的功能

--- src/main/java/cn/tuchangle/web/FoodDiaryRepository.java
+++ src/main/java/cn/tuchangle/web/FoodDiaryRepository.java
@@ -1,7 +1,14 @@
 package cn.tuchangle.web;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public interface FoodDiaryRepository extends CrudRepository<FoodDiary, Integer> {
+
+  List<FoodDiary> findByDate(@Param("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 
 }
