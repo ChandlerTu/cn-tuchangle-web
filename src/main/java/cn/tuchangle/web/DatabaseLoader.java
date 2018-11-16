673ba41517ca46e5183e93e960ced70dee4b2c31
Chandler Tu <chandlertu@chandlertu.com>
2018-11-16 15:44:50 +0800

    修改初始化数据的日期时间为零点

--- src/main/java/cn/tuchangle/web/DatabaseLoader.java
+++ src/main/java/cn/tuchangle/web/DatabaseLoader.java
@@ -1,5 +1,6 @@
 package cn.tuchangle.web;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,22 +15,29 @@ public class DatabaseLoader implements CommandLineRunner {
 
   @Override
   public void run(String... strings) throws Exception {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.HOUR_OF_DAY, 0);
+    calendar.set(Calendar.MINUTE, 0);
+    calendar.set(Calendar.SECOND, 0);
+    calendar.set(Calendar.MILLISECOND, 0);
+    Date date = calendar.getTime();
+
     FoodDiary foodDiary1 = new FoodDiary();
     foodDiary1.setFood("苹果");
     foodDiary1.setGram(100);
-    foodDiary1.setDate(new Date());
+    foodDiary1.setDate(date);
     this.repository.save(foodDiary1);
 
     FoodDiary foodDiary2 = new FoodDiary();
     foodDiary2.setFood("香蕉");
     foodDiary2.setGram(200);
-    foodDiary2.setDate(new Date());
+    foodDiary2.setDate(date);
     this.repository.save(foodDiary2);
 
     FoodDiary foodDiary3 = new FoodDiary();
     foodDiary3.setFood("葡萄");
     foodDiary3.setGram(300);
-    foodDiary3.setDate(new Date());
+    foodDiary3.setDate(date);
     this.repository.save(foodDiary3);
   }
 
