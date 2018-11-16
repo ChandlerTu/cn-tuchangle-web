e53c01206bffffaacf4d11ecfaf1e08fa5b34c3a
Chandler Tu <chandlertu@chandlertu.com>
2018-11-16 15:44:21 +0800

    解决 Spring Boot 整合 Vue Router 的 Url 问题

--- src/main/java/cn/tuchangle/web/CnTuchangleWebApplication.java
+++ src/main/java/cn/tuchangle/web/CnTuchangleWebApplication.java
@@ -2,6 +2,11 @@ package cn.tuchangle.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class CnTuchangleWebApplication {
@@ -9,4 +14,10 @@ public class CnTuchangleWebApplication {
   public static void main(String[] args) {
     SpringApplication.run(CnTuchangleWebApplication.class, args);
   }
+
+  @Bean
+  public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+    return factory -> factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
+  }
+
 }
